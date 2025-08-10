48f2a67fb7f34606759b000e9dfe6fbdf6871794
Peter <phorrigan@gmail.com>
2025-08-10 01:01:51 -0400

    Update user.ts

--- user.ts
+++ user.ts
@@ -1,17 +1,23 @@
 import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
 import { SaaSMapping } from './SaaSMapping';
 
-@Entity()
+@Entity({ name: 'users' })
 export class User {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Column({ unique: true })
+  @Column({ type: 'varchar', unique: true })
   email: string;
 
-  @Column({ nullable: true })
+  @Column({ type: 'varchar', nullable: true })
   githubId?: string;
 
-  @OneToMany(() => SaaSMapping, (mapping) => mapping.user)
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP', onUpdate: 'CURRENT_TIMESTAMP' })
+  updatedAt: Date;
+
+  @OneToMany(() => SaaSMapping, mapping => mapping.user, { cascade: true })
   saasMappings: SaaSMapping[];
 }
